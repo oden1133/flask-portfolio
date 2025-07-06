bcee1da0c03c52ab0b0c5ebf40e91256ec0c08de
kaito <kaitototo3307@gmail.com>
2025-07-06 18:02:31 +0900

    Add HTML and CSS files

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>はじめてのポートフォリオ</h1>
-    <p>こんにちは！農学部で統計を学んでいる大学生です。</p>
-    <p>PythonとFlaskを使って、自分の紹介ページを作っています！</p>
-    """
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=True)
